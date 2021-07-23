8cb120765af5f3f69bac77f687c2cb5bdaa01c26
GitHub Action <action@github.com>
2021-07-23 02:28:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/sculiuzy/firstplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sculiuzy/firstplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sculiuzy.firstplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sculiuzy.firstplugin.MyBundle
 
 class MyApplicationService {
 
